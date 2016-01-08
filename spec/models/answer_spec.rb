c5246ed563e8523bf5886be1fec1af83710a5b84
Anthony Hart <cidsolutions@yahoo.com>
2016-01-07 22:33:05 -0800

    Assignment 28 Models: Complete

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: "Resolved Answer")}
+  let(:answer) {Answer.create!(body: "New Answer Body")}
+
+  describe "attributes" do
+
+    it "should respond to body" do
+      expect(answer).to respond_to(:body)
+    end
+
+  end
+
+end
