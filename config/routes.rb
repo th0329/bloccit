c9d6a24165b88825840d2a25958c94391d44b9c4
Anthony Hart <cidsolutions@yahoo.com>
2016-01-19 21:23:56 -0800

    Assignment 33 CRUD Questions: Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :questions
+  get 'questions/show'
+
+
   resources :posts
 
   get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,16 @@ posts = Post.all
   )
 end
 
+100.times do
+  Question.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  resolved: false
+  )
+
+end
+
 puts "Seed Finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
