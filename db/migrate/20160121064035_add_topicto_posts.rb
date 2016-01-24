56c878c6441a060757c5edabe65e091faa552502
Anthony Hart <cidsolutions@yahoo.com>
2016-01-23 17:51:44 -0800

    Checkpoint 34 CRUD More: Complete

---
+++ db/migrate/20160121064035_add_topicto_posts.rb
@@ -0,0 +1,4 @@
+class AddTopictoPosts < ActiveRecord::Migration
+  def change
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
-  let(:post) {Post.create!(title: "New Post Title", body: "New post Body")}
+  let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+  let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+
+  it { should belong_to(:topic) }
 
   describe "attributes" do
 
@@ -9,10 +13,10 @@ RSpec.describe Post, type: :model do
       expect(post).to respond_to(:title)
     end
 
-    it "shiould respond to body" do
+    it "should respond to body" do
       expect(post).to respond_to(:body)
     end
 
   end
-  
+
 end
