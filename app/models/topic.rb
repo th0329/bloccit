30598e2e1ebfa53ccb232dc544d8986500e43640
Anthony Hart <cidsolutions@yahoo.com>
2016-01-27 22:57:53 -0800

    Assignment 35 Validations: Complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :sponsoredposts
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
+
 end
