fe18133fd349d4c65095674a4d10155cbe414c31
Anthony Hart <cidsolutions@yahoo.com>
2016-02-04 21:59:01 -0800

    Checkpoint 40 Authorization: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -20,5 +21,7 @@ class User < ActiveRecord::Base
             format: { with: EMAIL_REGEX }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204010310) do
+ActiveRecord::Schema.define(version: 20160205050405) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20160204010310) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20160204010310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160204010310) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
