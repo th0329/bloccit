7cef393c804780d96d1887b22a78e40571c3fe21
Anthony Hart <cidsolutions@yahoo.com>
2016-02-26 13:37:10 -0800

    Checkpoint 49 API Send: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,10 +38,12 @@ class User < ActiveRecord::Base
   end
 
   def generate_auth_token
+
     loop do
       self.auth_token = SecureRandom.base64(64)
       break unless User.find_by(auth_token: auth_token)
     end
+
   end
 
 end
